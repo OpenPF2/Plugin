9c2f4867e6b7356b8d66810a6188d516749f4156
Guy Elsmore-Paddock <guy@inveniem.com>
2021-05-09 16:49:11 -0400

    Upgrade to New UE 4.24+ Build Settings

--- Source/TeenWolfRpg/Public/TeenWolfRpg.h
+++ Source/TeenWolfRpg/Public/TeenWolfRpg.h
@@ -7,9 +7,9 @@
 
 #pragma once
 
-#include "EngineMinimal.h"
+#include <EngineMinimal.h>
 #include "Engine/Engine.h"
-#include "Net/UnrealNetwork.h"
+#include <Net/UnrealNetwork.h>
 
 TEENWOLFRPG_API DECLARE_LOG_CATEGORY_EXTERN(LogTwRpg, Log, All);
 TEENWOLFRPG_API DECLARE_LOG_CATEGORY_EXTERN(LogTwRpgStatsDebug, VeryVerbose, VeryVerbose);
