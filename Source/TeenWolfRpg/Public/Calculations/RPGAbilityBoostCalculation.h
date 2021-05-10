888932ded1c8dd3053a5286461821f8c907bd976
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2021-05-10 01:34:56 -0400

    Implement Support for P2E Ability Boosts
    Boosts add +2 to abilities < 18, or +1 to abilities >= 18. They can also
    be stacked.
    
    Content/OpenPathfinder2e/Core/GameEffect/GE_BoostAbilities right now is
    a demo of how to apply them to a character. Ideally we should have a
    more flexible Blueprint or something we can supply parameters/values to
    that can handle this instead.

---
+++ Source/TeenWolfRpg/Public/Calculations/RPGAbilityBoostCalculation.h
@@ -0,0 +1,45 @@
+﻿// Teen Wolf RPG game logic, Copyright 2021, Teen Wolf RPG. All Rights Reserved.
+// Content from Pathfinder 2nd Edition is licensed under the Open Game License (OGL) v1.0a, subject to the following:
+//	 - Open Game License v 1.0a, Copyright 2000, Wizards of the Coast, Inc.
+//	 - System Reference Document, Copyright 2000, Wizards of the Coast, Inc.
+//	 - Pathfinder Core Rulebook (Second Edition), Copyright 2019, Paizo Inc.
+// Except for material designated as Product Identity or External Tools, the game mechanics of this Teen Wolf RPG
+// product are Open Game Content, as defined in the Open Game License version 1.0a, Section 1(d) (see accompanying
+// LICENSE.TXT). No portion of this work other than the material designated as Open Game Content may be reproduced in
+// any form without written permission.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "GameplayModMagnitudeCalculation.h"
+#include "RPGAbilityCalculationBase.h"
+
+#include "RPGAbilityBoostCalculation.generated.h"
+
+/**
+ * An MMC for adding points to an ability score.
+ */
+UCLASS()
+class TEENWOLFRPG_API URPGAbilityBoostCalculation : public URPGAbilityCalculationBase
+{
+	GENERATED_BODY()
+
+	/**
+	 * Calculates an ability boost.
+	 *
+	 * From the Pathfinder 2E Core Rulebook, page 20:
+	 * "An ability boost normally increases an ability score’s value by 2. However, if the ability score to which you’re
+	 * applying an ability boost is already 18 or higher, its value increases by only 1. At 1st level, a character can
+	 * never have any ability score that’s higher than 18."
+	 *
+	 * @param AbilityAttribute
+	 *   The type of ability score for which an ability boost is desired.
+	 * @param AbilityScore
+	 *   The current base value of the ability attribute.
+	 *
+	 * @return
+	 *   The boost to apply to the ability score. (This method does not add the ability score to the boost).
+	 */
+	virtual float DoCalculation(const FGameplayAttribute AbilityAttribute, const float AbilityScore) const override;
+};
