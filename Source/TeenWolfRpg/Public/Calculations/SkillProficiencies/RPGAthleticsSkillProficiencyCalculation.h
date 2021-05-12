db71bfffee9ca4366e7cdee9794a5ccbcd846c7a
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2021-05-12 02:09:12 -0400

    WIP Skill Proficiency Bonus Calculation

---
+++ Source/TeenWolfRpg/Public/Calculations/SkillProficiencies/RPGAthleticsSkillProficiencyCalculation.h
@@ -0,0 +1,20 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include <CoreMinimal.h>
+
+#include "RPGSkillProficiencyCalculationBase.h"
+#include "RPGAthleticsSkillProficiencyCalculation.generated.h"
+
+/**
+* MMC that calculates the proficiency a character has in Athletics skill.
+*/
+UCLASS()
+class TEENWOLFRPG_API URPGAthleticsSkillProficiencyCalculation : public URPGSkillProficiencyCalculationBase
+{
+	GENERATED_BODY()
+
+public:
+	explicit URPGAthleticsSkillProficiencyCalculation();
+};
