6eb6990e5d7b16fc0b4ee33dcce6ba03b78219c4
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2021-07-11 20:13:33 -0400

    Rename "Proficiencies" to "Modifiers"
    Most things on the stats sheet are actually modifiers, not the raw
    proficiency score itself.

---
+++ Source/TeenWolfRpg/Public/Calculations/Modifiers/Skills/RPGSurvivalModifierCalculation.h
@@ -0,0 +1,28 @@
+﻿// Teen Wolf RPG game logic, Copyright 2021, Teen Wolf RPG. All Rights Reserved.
+// Content from Pathfinder 2nd Edition is licensed under the Open Game License (OGL) v1.0a, subject to the following:
+//	 - Open Game License v 1.0a, Copyright 2000, Wizards of the Coast, Inc.
+//	 - System Reference Document, Copyright 2000, Wizards of the Coast, Inc.
+//	 - Pathfinder Core Rulebook (Second Edition), Copyright 2019, Paizo Inc.
+// Except for material designated as Product Identity or External Tools, the game mechanics of this Teen Wolf RPG
+// product are Open Game Content, as defined in the Open Game License version 1.0a, Section 1(d) (see accompanying
+// LICENSE.TXT). No portion of this work other than the material designated as Open Game Content may be reproduced in
+// any form without written permission.
+
+#pragma once
+
+#include <CoreMinimal.h>
+
+#include "Calculations/Modifiers/RPGModifierCalculationBase.h"
+#include "RPGSurvivalModifierCalculation.generated.h"
+
+/**
+ * MMC that calculates the proficiency a character has in Survival skill.
+ */
+UCLASS()
+class TEENWOLFRPG_API URPGSurvivalModifierCalculation : public URPGModifierCalculationBase
+{
+	GENERATED_BODY()
+
+public:
+	explicit URPGSurvivalModifierCalculation();
+};
