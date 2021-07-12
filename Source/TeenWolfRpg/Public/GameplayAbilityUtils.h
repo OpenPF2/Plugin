4df1d3af584d56971ec6c051abdd1a3afe89b3d5
Guy Elsmore-Paddock <guy@inveniem.com>
2021-07-12 02:21:48 -0400

    Move Tag Presence Checking into New GameplayAbilityUtils Class
    This will allow us to re-use this logic elsewhere.

---
+++ Source/TeenWolfRpg/Public/GameplayAbilityUtils.h
@@ -0,0 +1,33 @@
+﻿// Teen Wolf RPG game logic, Copyright 2021, Teen Wolf RPG. All Rights Reserved.
+
+//=============================================================================
+// GameplayAbilityUtils.h: Utility logic for working with Gameplay Abilities.
+//=============================================================================
+
+#pragma once
+
+#include <CoreMinimal.h>
+
+#include "GameplayEffectTypes.h"
+#include "GameplayTagContainer.h"
+
+namespace GameplayAbilityUtils
+{
+	/**
+	 * Checks if a tag with the given name or prefix is present.
+	 *
+	 * @param Tags
+	 *   The list of tags in which to search.
+	 * @param TagNameOrPrefix
+	 *   The name of the tag or the prefix.
+	 *
+	 * @return
+	 *   - TRUE if given a tag name, and a tag with the specified name is present in the tag list.
+	 *   - TRUE if given a tag prefix, and there is a tag present in the tag list that starts with that prefix.
+	 *   - FALSE, otherwise.
+	 */
+	FORCEINLINE static bool HasTag(const FGameplayTagContainer *Tags, const FString TagNameOrPrefix)
+	{
+		return Tags->HasTag(FGameplayTag::RequestGameplayTag(FName(TagNameOrPrefix)));
+	}
+}
