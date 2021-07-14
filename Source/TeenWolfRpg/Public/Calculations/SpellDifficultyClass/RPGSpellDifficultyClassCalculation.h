13ff536ea8bd85361a5e5d91619b22b6a965ad0e
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2021-07-14 01:21:14 -0400

    Implement MMC for Spell DC

---
+++ Source/TeenWolfRpg/Public/Calculations/SpellDifficultyClass/RPGSpellDifficultyClassCalculation.h
@@ -0,0 +1,40 @@
+﻿// Teen Wolf RPG game logic, Copyright 2021, Teen Wolf RPG. All Rights Reserved.
+// Content from Pathfinder 2nd Edition is licensed under the Open Game License (OGL) v1.0a, subject to the following:
+//	 - Open Game License v 1.0a, Copyright 2000, Wizards of the Coast, Inc.
+//	 - System Reference Document, Copyright 2000, Wizards of the Coast, Inc.
+//	 - Pathfinder Core Rulebook (Second Edition), Copyright 2019, Paizo Inc.
+// Except for material designated as Product Identity or External Tools, the game mechanics of this Teen Wolf RPG
+// product are Open Game Content, as defined in the Open Game License version 1.0a, Section 1(d) (see accompanying
+// LICENSE.TXT). No portion of this work other than the material designated as Open Game Content may be reproduced in
+// any form without written permission.
+
+#pragma once
+
+#include <CoreMinimal.h>
+
+#include "Calculations/RPGKeyAbilityCalculationBase.h"
+#include "RPGSpellDifficultyClassCalculation.generated.h"
+
+/**
+ * MMC that calculates the "Spell DC" stat.
+ *
+ * From the Pathfinder 2E Core Rulebook, page 448:
+ * "Many times, instead of requiring you to make a spell attack roll, the spells you cast will require those within the
+ * area or targeted by the spell to attempt a saving throw against your Spell DC to determine how the spell affects
+ * them.
+ *
+ * Your spell DC is calculated using the following formula.
+ * Spell DC =
+ * 10 + ability modifier used for spellcasting + proficiency bonus + other bonuses + penalties"
+ */
+UCLASS()
+class TEENWOLFRPG_API URPGSpellDifficultyClassCalculation : public URPGKeyAbilityCalculationBase
+{
+	GENERATED_BODY()
+
+public:
+	/**
+	 * Constructor for URPGSpellDifficultyClassCalculation.
+	 */
+	explicit URPGSpellDifficultyClassCalculation();
+};
