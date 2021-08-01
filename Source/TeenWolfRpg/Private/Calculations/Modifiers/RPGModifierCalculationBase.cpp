6374308aa172bf6ed0f264ddd0089f257fcff1e7
Guy Elsmore-Paddock <guy@inveniem.com>
2021-08-01 16:23:22 -0400

    Unify TEML-based Calculations into a Base Class

--- Source/TeenWolfRpg/Private/Calculations/Modifiers/RPGModifierCalculationBase.cpp
+++ Source/TeenWolfRpg/Private/Calculations/Modifiers/RPGModifierCalculationBase.cpp
@@ -15,7 +15,7 @@
 
 URPGModifierCalculationBase::URPGModifierCalculationBase(const FGameplayAttribute SkillAbilityAttribute,
 														 const FString SkillGameplayTagPrefix) :
-	UGameplayModMagnitudeCalculation()
+	URPGTemlCalculationBase()
 {
 	this->SkillAbilityCaptureDefinition.AttributeToCapture	= SkillAbilityAttribute;
 	this->SkillAbilityCaptureDefinition.AttributeSource		= EGameplayEffectAttributeCaptureSource::Source;
@@ -29,7 +29,8 @@ URPGModifierCalculationBase::URPGModifierCalculationBase(const FGameplayAttribut
 float URPGModifierCalculationBase::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
 {
 	float							AbilityScore			= 0.0f,
-									ProficiencyBonus;
+									ProficiencyBonus,
+									Modifier;
 	const FGameplayTagContainer		*SourceTags				= Spec.CapturedSourceTags.GetAggregatedTags(),
 									*TargetTags				= Spec.CapturedTargetTags.GetAggregatedTags();
 	FAggregatorEvaluateParameters	EvaluationParameters;
@@ -39,60 +40,26 @@ float URPGModifierCalculationBase::CalculateBaseMagnitude_Implementation(const F
 
 	GetCapturedAttributeMagnitude(this->SkillAbilityCaptureDefinition, Spec, EvaluationParameters, AbilityScore);
 
-	// Source for all that follows: Pathfinder 2E Core Rulebook, page 444,
-	// "Step 1: Roll D20 and Identify The Modifiers, Bonuses, and Penalties That Apply".
+	// "In the second box to the right of each skill name on your character sheet, there’s an abbreviation that reminds
+	// you of the ability score tied to that skill. For each skill in which your character is trained, add your
+	// proficiency bonus for that skill (typically +3 for a 1st-level character) to the indicated ability’s modifier, as
+	// well as any other applicable bonuses and penalties, to determine the total modifier for that skill. For skills
+	// your character is untrained in, use the same method, but your proficiency bonus is +0."
 	//
-	// Start with a bonus equal to the character's score in the ability that influences the skill (e.g. Dexterity
-	// affects Acrobatics, Intelligence affects Arcana, etc.)
-	ProficiencyBonus = AbilityScore;
+	// Source: Pathfinder 2E Core Rulebook, page 28, "Skills".
+	ProficiencyBonus = CalculateProficiencyBonus(this->SkillGameplayTagPrefix, Spec);
 
-	// Bypass additional checks if the character has no proficiency with this skill, to avoid checking every TEML
-	// option.
-	if (GameplayAbilityUtils::HasTag(SourceTags, this->SkillGameplayTagPrefix))
-	{
-		const float CharacterLevel = Spec.GetLevel();
-
-		// "When attempting a check that involves something you have some training in, you will also add your
-		// proficiency bonus. This bonus depends on your proficiency rank: untrained, trained, expert, master, or
-		// legendary. If you’re untrained, your bonus is +0—you must rely on raw talent and any bonuses from the
-		// situation. Otherwise, the bonus equals your character’s level plus a certain amount depending on your rank.
-		// If your proficiency rank is trained, this bonus is equal to your level + 2, and higher proficiency ranks
-		// further increase the amount you add to your level."
-		if (GameplayAbilityUtils::HasTag(SourceTags, this->SkillGameplayTagPrefix + ".Legendary"))
-		{
-			// Legendary -> Your level + 8
-			ProficiencyBonus += CharacterLevel + 8;
-		}
-		else if (GameplayAbilityUtils::HasTag(SourceTags, this->SkillGameplayTagPrefix + ".Master"))
-		{
-			// Master -> Your level + 6
-			ProficiencyBonus += CharacterLevel + 6;
-		}
-		else if (GameplayAbilityUtils::HasTag(SourceTags, this->SkillGameplayTagPrefix + ".Expert"))
-		{
-			// Expert -> Your level + 4
-			ProficiencyBonus += CharacterLevel + 4;
-		}
-		else if (GameplayAbilityUtils::HasTag(SourceTags, this->SkillGameplayTagPrefix + ".Trained"))
-		{
-			// Trained -> Your level + 2
-			ProficiencyBonus += CharacterLevel + 2;
-		}
-		else
-		{
-			// Untrained -> No bonus at all, no matter what level.
-			ProficiencyBonus += 0;
-		}
-	}
+	Modifier = AbilityScore + ProficiencyBonus;
 
 	UE_LOG(
 		LogTwRpg,
 		VeryVerbose,
-		TEXT("Calculated skill proficiency/modifier ('%s': %f): %f"),
+		TEXT("Calculated modifier ('%s'): %f + %f = %f"),
 		*(this->SkillGameplayTagPrefix),
 		AbilityScore,
-		ProficiencyBonus
+		ProficiencyBonus,
+		Modifier
 	);
 
-	return ProficiencyBonus;
+	return Modifier;
 }
