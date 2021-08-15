444f9238b504d15bae6eccc0e10d896b7c94eb6e
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2021-08-14 20:53:43 -0400

    Code Clean-up -- Remove Needless Single-file Folders
    This moves code like AC and Class DC calculations up into the
    "Calculations" folder, since having them nested in their own folders was
    needless since they individual, single files.

--- Source/OpenPF2Core/Private/Calculations/PF2ClassDifficultyClassCalculation.cpp
+++ Source/OpenPF2Core/Private/Calculations/PF2ClassDifficultyClassCalculation.cpp
@@ -3,9 +3,9 @@
 // This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 // distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-#include "Calculations/SpellAttackRoll/PF2SpellAttackRollCalculation.h"
+#include "Calculations/PF2ClassDifficultyClassCalculation.h"
 
-UPF2SpellAttackRollCalculation::UPF2SpellAttackRollCalculation() :
-	UPF2KeyAbilityCalculationBase(TEXT("SpellAttack"), TEXT("SpellcastingKeyAbility"))
+UPF2ClassDifficultyClassCalculation::UPF2ClassDifficultyClassCalculation() :
+	UPF2KeyAbilityCalculationBase(TEXT("ClassDc"), TEXT("KeyAbility"), 10.0f)
 {
 }
