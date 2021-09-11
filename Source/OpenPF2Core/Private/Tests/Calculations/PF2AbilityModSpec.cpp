37c26c17e5ae62383a4cb6231bb55f57bb436d51
Guy Elsmore-Paddock <guy@inveniem.com>
2021-09-10 22:48:48 -0400

    Fix Licensing on Two Files
    `PF2SpecBase` is based on code from Epic, so let's err on the side of
    caution. `PF2AbilityModSpec`, meanwhile, references some data from the
    PF2 core rulebook, so best to license that under the OGL.

--- Source/OpenPF2Core/Private/Tests/Calculations/PF2AbilityModSpec.cpp
+++ Source/OpenPF2Core/Private/Tests/Calculations/PF2AbilityModSpec.cpp
@@ -1,7 +1,13 @@
 ﻿// OpenPF2 for UE Game Logic, Copyright 2021, Guy Elsmore-Paddock. All Rights Reserved.
 //
-// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
-// distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+// Content from Pathfinder 2nd Edition is licensed under the Open Game License (OGL) v1.0a, subject to the following:
+//	 - Open Game License v 1.0a, Copyright 2000, Wizards of the Coast, Inc.
+//	 - System Reference Document, Copyright 2000, Wizards of the Coast, Inc.
+//	 - Pathfinder Core Rulebook (Second Edition), Copyright 2019, Paizo Inc.
+// Except for material designated as Product Identity, the game mechanics and logic in this file are Open Game Content,
+// as defined in the Open Game License version 1.0a, Section 1(d) (see accompanying LICENSE.TXT). No portion of this
+// file other than the material designated as Open Game Content may be reproduced in any form without written
+// permission.
 
 #include "Abilities/PF2AttributeSet.h"
 #include "Calculations/PF2AbilityModifierCalculation.h"
