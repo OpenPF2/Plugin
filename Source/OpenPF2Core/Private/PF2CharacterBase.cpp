537d2cb5974b840e1d5cab85f26a6e68f3e48368
Guy Elsmore-Paddock <guy@inveniem.com>
2021-09-25 00:21:49 -0400

    Initial Skeleton of Base Class for PF2 Characters
    This is just the bare bones, taking inspiration from what Epic's Action
    RPG code is doing. This version does not handle inventory, abilities, or
    tags (yet).

---
+++ Source/OpenPF2Core/Private/PF2CharacterBase.cpp
@@ -0,0 +1,134 @@
+// OpenPF2 for UE Game Logic, Copyright 2021, Guy Elsmore-Paddock. All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+// distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "PF2CharacterBase.h"
+#include "Abilities/PF2AbilitySystemComponent.h"
+
+#include <AbilitySystemGlobals.h>
+#include <Net/UnrealNetwork.h>
+
+APF2CharacterBase::APF2CharacterBase()
+{
+	UPF2AbilitySystemComponent* NewAbilitySystemComponent =
+		this->CreateDefaultSubobject<UPF2AbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+
+	NewAbilitySystemComponent->SetIsReplicated(true);
+
+	this->AbilitySystemComponent     = NewAbilitySystemComponent;
+	this->AttributeSet               = this->CreateDefaultSubobject<UPF2AttributeSet>(TEXT("AttributeSet"));
+	this->CharacterLevel             = 1;
+	this->bPassiveEffectsInitialized = false;
+	this->CharacterName              = FText::FromString(TEXT("Character"));
+}
+
+void APF2CharacterBase::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+
+	if (this->AbilitySystemComponent)
+	{
+		this->AbilitySystemComponent->InitAbilityActorInfo(this, this);
+		this->AddStartupPassiveGameplayEffects();
+	}
+}
+
+void APF2CharacterBase::OnRep_Controller()
+{
+	Super::OnRep_Controller();
+
+	if (this->AbilitySystemComponent)
+	{
+		this->AbilitySystemComponent->RefreshAbilityActorInfo();
+	}
+}
+
+void APF2CharacterBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(APF2CharacterBase, CharacterLevel);
+}
+
+UAbilitySystemComponent* APF2CharacterBase::GetAbilitySystemComponent() const
+{
+	return this->AbilitySystemComponent;
+}
+
+int32 APF2CharacterBase::GetCharacterLevel() const
+{
+	return this->CharacterLevel;
+}
+
+bool APF2CharacterBase::SetCharacterLevel(const int32 NewLevel)
+{
+	const int32 OldLevel = this->CharacterLevel;
+
+	if ((OldLevel != NewLevel) && (NewLevel > 0))
+	{
+		this->HandleCharacterLevelChanged(OldLevel, NewLevel);
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void APF2CharacterBase::AddStartupPassiveGameplayEffects()
+{
+	check(this->AbilitySystemComponent);
+
+	if ((GetLocalRole() == ROLE_Authority) && !this->bPassiveEffectsInitialized)
+	{
+		for (const TSubclassOf<UGameplayEffect>& GameplayEffect : this->PassiveGameplayEffects)
+		{
+			FGameplayEffectContextHandle EffectContext = this->AbilitySystemComponent->MakeEffectContext();
+			FGameplayEffectSpecHandle    NewHandle;
+
+			EffectContext.AddSourceObject(this);
+
+			NewHandle = this->AbilitySystemComponent->MakeOutgoingSpec(
+				GameplayEffect,
+				this->GetCharacterLevel(),
+				EffectContext
+			);
+
+			if (NewHandle.IsValid())
+			{
+				this->AbilitySystemComponent->ApplyGameplayEffectSpecToTarget(
+					*NewHandle.Data.Get(),
+					this->AbilitySystemComponent
+				);
+			}
+		}
+
+		this->bPassiveEffectsInitialized = true;
+	}
+}
+
+void APF2CharacterBase::RemoveStartupPassiveGameplayEffects()
+{
+	check(this->AbilitySystemComponent);
+
+	if ((GetLocalRole() == ROLE_Authority) && this->bPassiveEffectsInitialized)
+	{
+		FGameplayEffectQuery Query;
+
+		Query.EffectSource = this;
+
+		this->AbilitySystemComponent->RemoveActiveEffects(Query);
+
+		this->bPassiveEffectsInitialized = false;
+	}
+}
+
+void APF2CharacterBase::HandleCharacterLevelChanged(const float OldLevel, const float NewLevel)
+{
+	this->RemoveStartupPassiveGameplayEffects();
+
+	this->CharacterLevel = NewLevel;
+
+	this->AddStartupPassiveGameplayEffects();
+}
---
+++ Source/OpenPF2Core/Public/Abilities/PF2AbilitySystemComponent.h
@@ -0,0 +1,18 @@
+// OpenPF2 for UE Game Logic, Copyright 2021, Guy Elsmore-Paddock. All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+// distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include <AbilitySystemComponent.h>
+#include "PF2AbilitySystemComponent.generated.h"
+
+UCLASS()
+class OPENPF2CORE_API UPF2AbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+
+public:
+	UPF2AbilitySystemComponent();
+};
