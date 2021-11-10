41f4768c1e8b6ff1e4d821c9d704d7345ce7cb00
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2021-11-10 01:45:10 -0500

    Implement Simple Dice Rolling Logic to Support PF2 Rules
    PF2 does not appear to rely on any of the complex dice roll expressions
    normally associated with other RPG systems like DnD, so this function
    library should be enough to handle the PF2 Core Rulebook. If not, we can
    make this more elaborate later.

---
+++ Source/OpenPF2Core/Private/PF2DiceLibrary.cpp
@@ -0,0 +1,65 @@
+﻿// OpenPF2 for UE Game Logic, Copyright 2021, Guy Elsmore-Paddock. All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+// distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "PF2DiceLibrary.h"
+
+#include "PF2ArrayUtilities.h"
+
+const FRegexPattern UPF2DiceLibrary::DiceRollPattern = FRegexPattern(TEXT("^(\\d{1,})d(\\d{1,})$"));
+
+int32 UPF2DiceLibrary::RollStringSum(const FString RollExpression)
+{
+	return PF2ArrayUtilities::Reduce(
+		RollString(RollExpression),
+		0,
+		[](const int32 PreviousValue, const int32 CurrentValue)
+		{
+			return PreviousValue + CurrentValue;
+		});
+}
+
+int32 UPF2DiceLibrary::RollSum(const int32 RollCount, const int32 SideCount)
+{
+	return PF2ArrayUtilities::Reduce(
+		Roll(RollCount, SideCount),
+		0,
+		[](const int32 PreviousValue, const int32 CurrentValue)
+		{
+			return PreviousValue + CurrentValue;
+		});
+}
+
+TArray<int32> UPF2DiceLibrary::RollString(const FString RollExpression)
+{
+	FRegexMatcher ExpressionMatcher = FRegexMatcher(DiceRollPattern, RollExpression);
+
+	if (ExpressionMatcher.FindNext())
+	{
+		const FString RollCountString = ExpressionMatcher.GetCaptureGroup(1),
+		              SideCountString = ExpressionMatcher.GetCaptureGroup(2);
+		const int32   RollCount       = FCString::Atoi(*RollCountString),
+		              SideCount       = FCString::Atoi(*SideCountString);
+
+		return Roll(RollCount, SideCount);
+	}
+	else
+	{
+		return {};
+	}
+}
+
+TArray<int32> UPF2DiceLibrary::Roll(const int32 RollCount, const int32 SideCount)
+{
+	TArray<int32> Rolls;
+
+	for (int32 RollIndex = 0; RollIndex < RollCount; ++RollIndex)
+	{
+		int32 Roll = FMath::RandRange(1, SideCount);
+
+		Rolls.Add(Roll);
+	}
+
+	return Rolls;
+}
---
+++ Source/OpenPF2Core/Public/PF2DiceLibrary.h
@@ -0,0 +1,97 @@
+﻿// OpenPF2 for UE Game Logic, Copyright 2021, Guy Elsmore-Paddock. All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+// distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include "Internationalization/Regex.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+
+#include "PF2DiceLibrary.generated.h"
+
+/**
+ * Simple function library for rolling dice of any size.
+ *
+ * Dice rolls can either be done by specifying the number of sides and number of rolls numerically; or by describing the
+ * roll in a string format like "2d6" (for rolling two, six-sided dice). For example, the following calls would produce
+ * equivalent results:
+ *	- RollString("2d6") and Roll(2,6) - Roll two, six-sided dice.
+ *	- RollString("1d4") and Roll(1,4) - Roll one, four-sided dice.
+ *	- RollString("10d12") and Roll(10,12) - Roll ten, twelve-sided dice.
+ *
+ * Unlike other RPG systems like Dungeons and Dragons, P2E game rules do *not* appear to require the ability to evaluate
+ * complex dice rolling expressions like "5d4+3", "3d6x10", or "d6/2". Consequently, these types of expressions are not
+ * supported at this time.
+ */
+UCLASS()
+class OPENPF2CORE_API UPF2DiceLibrary final : public UBlueprintFunctionLibrary {
+	GENERATED_BODY()
+
+protected:
+	/**
+	 * Regular expression pattern used to parse dice rolling expressions.
+	 */
+	static const FRegexPattern DiceRollPattern;
+
+public:
+	/**
+	 * Returns the sum of a dice roll for the given dice roll expression string.
+	 *
+	 * Complex roll expressions like "3d6x10" are not supported at this time.
+	 *
+	 * @param RollExpression
+	 *	The description of the roll, in "CdS" format, where "C" represents the count or number of dice to roll, and "S"
+	 *	represents the number of sides of each die. For example, "1d6" represents a single roll of a six-sided die,
+	 *	while "2d4" represents rolling two dice having four sides each.
+	 *
+	* @return
+	 *	The sum of the dice roll(s).
+	 */
+	UFUNCTION(BlueprintPure)
+	static int32 RollStringSum(const FString RollExpression);
+
+	/**
+	 * Returns the sum of a dice roll for the given numeric parameters.
+	 *
+	 * @param RollCount
+	 *	The count or number of dice to roll.
+	 * @param SideCount
+	 *	The number of sides of each die.
+	 *
+	 * @return
+	 *	The sum of the dice roll(s).
+	 */
+	UFUNCTION(BlueprintPure)
+	static int32 RollSum(const int32 RollCount, const int32 SideCount);
+
+	/**
+	 * Returns the result of a dice roll for the given dice roll expression string.
+	 *
+	 * Complex roll expressions like "3d6x10" are not supported at this time.
+	 *
+	 * @param RollExpression
+	 *	The description of the roll, in "CdS" format, where "C" represents the count or number of dice to roll, and "S"
+	 *	represents the number of sides of each die. For example, "1d6" represents a single roll of a six-sided die,
+	 *	while "2d4" represents rolling two dice having four sides each.
+	 *
+	* @return
+	 *	The result of each dice roll.
+	 */
+	UFUNCTION(BlueprintPure)
+	static TArray<int32> RollString(const FString RollExpression);
+
+	/**
+	 * Returns the result of a dice roll for the given numeric parameters.
+	 *
+	 * @param RollCount
+	 *	The count or number of dice to roll.
+	 * @param SideCount
+	 *	The number of sides of each die.
+	 *
+	 * @return
+	 *	The result of each dice roll.
+	 */
+	UFUNCTION(BlueprintPure)
+	static TArray<int32> Roll(const int32 RollCount, const int32 SideCount);
+};
