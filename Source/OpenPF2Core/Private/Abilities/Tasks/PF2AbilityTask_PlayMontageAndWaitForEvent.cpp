e70ab91b38026a91c719105445d22c7fba48794f
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2021-11-30 02:14:13 -0500

    Add PF2 Version of "Play Montage and Wait for Event" Ability Task
    This makes it much easier for us to play a montage that may return any
    one of several results (in the form of event tags).
    
    This is a cleaned-up version of an ability task with the same name from
    the Action RPG sample project. We should be safe to pull this into the
    project (from a licensing perspective), since the content carries the
    following note:
    
    > UE-Only Content - Licensed for Use Only with Unreal Engine-based
    > Products
    
    From:
    https://www.unrealengine.com/marketplace/en-US/product/action-rpg

---
+++ Source/OpenPF2Core/Private/Abilities/Tasks/PF2AbilityTask_PlayMontageAndWaitForEvent.cpp
@@ -0,0 +1,342 @@
+// Adapted from content that is Copyright Epic Games, Inc. All Rights Reserved. (Action RPG Sample)
+// Licensed only for use with Unreal Engine.
+
+#include "Abilities/Tasks/PF2AbilityTask_PlayMontageAndWaitForEvent.h"
+
+#include <GameFramework/Character.h>
+#include <AbilitySystemComponent.h>
+#include <AbilitySystemGlobals.h>
+#include <Animation/AnimInstance.h>
+#include "Abilities/PF2AbilitySystemComponent.h"
+
+UPF2AbilityTask_PlayMontageAndWaitForEvent* UPF2AbilityTask_PlayMontageAndWaitForEvent::PlayMontageAndWaitForEvent(
+	UGameplayAbility*           OwningAbility,
+	const FName                 TaskInstanceName,
+	UAnimMontage*               MontageToPlay,
+	const FGameplayTagContainer EventTags,
+	float                       Rate,
+	const FName                 StartSection,
+	const bool                  bStopWhenAbilityEnds,
+	const float                 AnimRootMotionTranslationScale)
+{
+	UAbilitySystemGlobals::NonShipping_ApplyGlobalAbilityScaler_Rate(Rate);
+
+	UPF2AbilityTask_PlayMontageAndWaitForEvent* Task =
+		NewAbilityTask<UPF2AbilityTask_PlayMontageAndWaitForEvent>(OwningAbility, TaskInstanceName);
+
+	// Sadly, we can't do this through the constructor because this is a UObject that UE handles construction on. So, we
+	// have to settle for setting the fields after construction.
+	Task->MontageToPlay                  = MontageToPlay;
+	Task->EventTags                      = EventTags;
+	Task->Rate                           = Rate;
+	Task->StartSection                   = StartSection;
+	Task->AnimRootMotionTranslationScale = AnimRootMotionTranslationScale;
+	Task->bStopWhenAbilityEnds           = bStopWhenAbilityEnds;
+
+	return Task;
+}
+
+UPF2AbilityTask_PlayMontageAndWaitForEvent::UPF2AbilityTask_PlayMontageAndWaitForEvent(
+	const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer), Rate(1.0f), bStopWhenAbilityEnds(true)
+{
+}
+
+void UPF2AbilityTask_PlayMontageAndWaitForEvent::Activate()
+{
+	if (this->Ability == nullptr)
+	{
+		return;
+	}
+
+	bool                        bPlayedMontage            = false;
+	UPF2AbilitySystemComponent* PF2AbilitySystemComponent = this->GetTargetAsc();
+
+	if (PF2AbilitySystemComponent != nullptr)
+	{
+		const FGameplayAbilityActorInfo* ActorInfo    = this->Ability->GetCurrentActorInfo();
+		UAnimInstance*                   AnimInstance = ActorInfo->GetAnimInstance();
+
+		if (AnimInstance != nullptr)
+		{
+			// Ask the ASC to notify us if a Gameplay Event with the given tag(s) is received.
+			this->EventHandle = PF2AbilitySystemComponent->AddGameplayEventTagContainerDelegate(
+				this->EventTags,
+				FGameplayEventTagMulticastDelegate::FDelegate::CreateUObject(
+					this,
+					&UPF2AbilityTask_PlayMontageAndWaitForEvent::OnGameplayEvent
+				)
+			);
+
+			const float MontageDuration =
+				PF2AbilitySystemComponent->PlayMontage(
+					this->Ability,
+					this->Ability->GetCurrentActivationInfo(),
+					this->MontageToPlay,
+					this->Rate,
+					this->StartSection
+				);
+
+			if (MontageDuration > 0.0f)
+			{
+				// Playing a montage could potentially fire off a callback into game code which could kill this ability!
+				// Early out if we are pending kill.
+				if (this->ShouldBroadcastAbilityTaskDelegates() == false)
+				{
+					return;
+				}
+
+				this->CancelledHandle =
+					this->Ability->OnGameplayAbilityCancelled.AddUObject(
+						this,
+						&UPF2AbilityTask_PlayMontageAndWaitForEvent::OnAbilityCancelled
+					);
+
+				this->BlendingOutDelegate.BindUObject(
+					this,
+					&UPF2AbilityTask_PlayMontageAndWaitForEvent::OnMontageBlendingOut
+				);
+
+				AnimInstance->Montage_SetBlendingOutDelegate(this->BlendingOutDelegate, this->MontageToPlay);
+
+				this->MontageEndedDelegate.BindUObject(
+					this,
+					&UPF2AbilityTask_PlayMontageAndWaitForEvent::OnMontageEnded
+				);
+
+				AnimInstance->Montage_SetEndDelegate(this->MontageEndedDelegate, this->MontageToPlay);
+
+				ACharacter*    Character = Cast<ACharacter>(this->GetAvatarActor());
+				const ENetRole LocalRole = Character->GetLocalRole();
+
+				if ((Character != nullptr) &&
+					(LocalRole == ROLE_Authority ||
+					 (LocalRole == ROLE_AutonomousProxy &&
+					  this->Ability->GetNetExecutionPolicy() == EGameplayAbilityNetExecutionPolicy::LocalPredicted)))
+				{
+					Character->SetAnimRootMotionTranslationScale(this->AnimRootMotionTranslationScale);
+				}
+
+				bPlayedMontage = true;
+			}
+		}
+		else
+		{
+			ABILITY_LOG(Warning, TEXT("UPF2AbilityTask_PlayMontageAndWaitForEvent call to PlayMontage failed!"));
+		}
+	}
+	else
+	{
+		ABILITY_LOG(
+			Warning,
+			TEXT("UPF2AbilityTask_PlayMontageAndWaitForEvent called on invalid AbilitySystemComponent.")
+		);
+	}
+
+	if (!bPlayedMontage)
+	{
+		ABILITY_LOG(
+			Warning,
+			TEXT("UPF2AbilityTask_PlayMontageAndWaitForEvent called in Ability %s failed to play montage %s; Task Instance Name %s."),
+			*this->Ability->GetName(),
+			*GetNameSafe(this->MontageToPlay),
+			*this->InstanceName.ToString()
+		);
+
+		if (ShouldBroadcastAbilityTaskDelegates())
+		{
+			this->OnCancelled.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+
+	this->SetWaitingOnAvatar();
+}
+
+void UPF2AbilityTask_PlayMontageAndWaitForEvent::ExternalCancel()
+{
+	check(this->AbilitySystemComponent);
+
+	this->OnAbilityCancelled();
+
+	Super::ExternalCancel();
+}
+
+void UPF2AbilityTask_PlayMontageAndWaitForEvent::OnDestroy(const bool AbilityEnded)
+{
+	// Note: Clearing montage end delegate isn't necessary since its not a multicast and will be cleared when the next
+	// montage plays. (If we are destroyed, it will detect this and not do anything)
+
+	// This delegate, however, should be cleared as it is a multicast
+	if (this->Ability != nullptr)
+	{
+		this->Ability->OnGameplayAbilityCancelled.Remove(this->CancelledHandle);
+
+		if (AbilityEnded && this->bStopWhenAbilityEnds)
+		{
+			// ReSharper disable once CppExpressionWithoutSideEffects
+			this->StopPlayingMontage();
+		}
+	}
+
+	UPF2AbilitySystemComponent* PF2AbilitySystemComponent = this->GetTargetAsc();
+
+	if (PF2AbilitySystemComponent)
+	{
+		PF2AbilitySystemComponent->RemoveGameplayEventTagContainerDelegate(this->EventTags, this->EventHandle);
+	}
+
+	Super::OnDestroy(AbilityEnded);
+}
+
+FString UPF2AbilityTask_PlayMontageAndWaitForEvent::GetDebugString() const
+{
+	const UAnimMontage* PlayingMontage = nullptr;
+
+	if (this->Ability != nullptr)
+	{
+		const FGameplayAbilityActorInfo* ActorInfo    = Ability->GetCurrentActorInfo();
+		UAnimInstance*                   AnimInstance = ActorInfo->GetAnimInstance();
+
+		if (AnimInstance != nullptr)
+		{
+			if (AnimInstance->Montage_IsActive(this->MontageToPlay))
+			{
+				PlayingMontage = this->MontageToPlay;
+			}
+			else
+			{
+				PlayingMontage = AnimInstance->GetCurrentActiveMontage();
+			}
+		}
+	}
+
+	FString DebugString =
+		FString::Printf(
+			TEXT("PlayMontageAndWaitForEvent. MontageToPlay: %s  (Currently Playing): %s"),
+			*GetNameSafe(this->MontageToPlay),
+			*GetNameSafe(PlayingMontage)
+		);
+
+	return DebugString;
+}
+
+UPF2AbilitySystemComponent* UPF2AbilityTask_PlayMontageAndWaitForEvent::GetTargetAsc() const
+{
+	return Cast<UPF2AbilitySystemComponent>(this->AbilitySystemComponent);
+}
+
+bool UPF2AbilityTask_PlayMontageAndWaitForEvent::StopPlayingMontage() const
+{
+	const FGameplayAbilityActorInfo* ActorInfo = this->Ability->GetCurrentActorInfo();
+
+	if (ActorInfo == nullptr)
+	{
+		return false;
+	}
+
+	UAnimInstance* AnimInstance = ActorInfo->GetAnimInstance();
+
+	if (AnimInstance == nullptr)
+	{
+		return false;
+	}
+
+	// Check if the montage is still playing
+	// The ability would have been interrupted, in which case we should automatically stop the montage
+	if ((this->AbilitySystemComponent != nullptr) && (this->Ability != nullptr))
+	{
+		if ((this->AbilitySystemComponent->GetAnimatingAbility() == this->Ability) &&
+			(this->AbilitySystemComponent->GetCurrentMontage() == this->MontageToPlay))
+		{
+			// Unbind delegates so they don't get called as well
+			FAnimMontageInstance* MontageInstance = AnimInstance->GetActiveInstanceForMontage(this->MontageToPlay);
+
+			if (MontageInstance != nullptr)
+			{
+				MontageInstance->OnMontageBlendingOutStarted.Unbind();
+				MontageInstance->OnMontageEnded.Unbind();
+			}
+
+			this->AbilitySystemComponent->CurrentMontageStop();
+
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void UPF2AbilityTask_PlayMontageAndWaitForEvent::OnGameplayEvent(const FGameplayTag        EventTag,
+                                                                 const FGameplayEventData* Payload) const
+{
+	if (this->ShouldBroadcastAbilityTaskDelegates())
+	{
+		FGameplayEventData TempData = *Payload;
+
+		TempData.EventTag = EventTag;
+
+		this->OnEventReceived.Broadcast(EventTag, TempData);
+	}
+}
+
+void UPF2AbilityTask_PlayMontageAndWaitForEvent::OnMontageEnded(UAnimMontage* Montage, const bool bInterrupted)
+{
+	if (!bInterrupted && (Montage == this->MontageToPlay))
+	{
+		if (this->ShouldBroadcastAbilityTaskDelegates())
+		{
+			this->OnCompleted.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+
+	this->EndTask();
+}
+
+void UPF2AbilityTask_PlayMontageAndWaitForEvent::OnMontageBlendingOut(UAnimMontage* Montage,
+                                                                      const bool    bInterrupted) const
+{
+	if ((this->Ability != nullptr) &&
+		(this->Ability->GetCurrentMontage() == this->MontageToPlay) &&
+		(Montage == this->MontageToPlay))
+	{
+		this->AbilitySystemComponent->ClearAnimatingAbility(this->Ability);
+
+		// Reset AnimRootMotionTranslationScale
+		ACharacter*    Character = Cast<ACharacter>(this->GetAvatarActor());
+		const ENetRole LocalRole = Character->GetLocalRole();
+
+		if ((Character != nullptr) &&
+			(LocalRole == ROLE_Authority ||
+			 (LocalRole == ROLE_AutonomousProxy &&
+			  this->Ability->GetNetExecutionPolicy() == EGameplayAbilityNetExecutionPolicy::LocalPredicted)))
+		{
+			Character->SetAnimRootMotionTranslationScale(1.0f);
+		}
+	}
+
+	if (bInterrupted)
+	{
+		if (this->ShouldBroadcastAbilityTaskDelegates())
+		{
+			this->OnInterrupted.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+	else
+	{
+		if (this->ShouldBroadcastAbilityTaskDelegates())
+		{
+			this->OnBlendOut.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+}
+
+void UPF2AbilityTask_PlayMontageAndWaitForEvent::OnAbilityCancelled() const
+{
+	if (this->StopPlayingMontage())
+	{
+		// Let the BP handle the interrupt as well
+		if (this->ShouldBroadcastAbilityTaskDelegates())
+		{
+			this->OnCancelled.Broadcast(FGameplayTag(), FGameplayEventData());
+		}
+	}
+}
