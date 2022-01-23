a3ead442e474376235ea2756052492e2ea3503b1
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2022-01-23 03:02:22 -0500

    [#7] First Working, Sequenced Execution of Queued Abilities during Enc.
    If we constrain the "Wait for Initiative Turn" ability task to only
    work for "Instanced per Execution" GAs, we can queue as many instances
    of a GA for a character as we want. At the same time, we can prevent the
    queued actions from firing off all at once by:
    
    1. Using blocking tags on the GAs, so that multiple GAs of the same type
       can't run concurrently. (The game designer has to set this up per
       GA).
    2. Toggling off "Should block other abilities" on the ability when the
       ability has been queued, so that the player/character can keep
       queuing up more instances of the ability without limitation.
    3. Checking at the time that the ability is dequeued if it is unblocked,
       relative to other GAs that are active on the character:
       - If it is not blocked, toggle "Should block other abilities" back on
         and execute it.
       - If it is blocked, put it back in the queue in the same position it
         was before and do not execute it.

--- Source/OpenPF2Core/Private/Abilities/Tasks/PF2AbilityTask_WaitForInitiativeTurn.cpp
+++ Source/OpenPF2Core/Private/Abilities/Tasks/PF2AbilityTask_WaitForInitiativeTurn.cpp
@@ -15,11 +15,11 @@
 #include "Utilities/PF2InterfaceUtilities.h"
 #include "Utilities/PF2LogUtilities.h"
 
-UPF2AbilityTask_WaitForInitiativeTurn* UPF2AbilityTask_WaitForInitiativeTurn::CreateWaitMovementModeChange(
-	UGameplayAbility* OwningAbility,
-	const FName       TaskInstanceName,
-	const FText       ActionName,
-	const FSlateBrush ActionIcon)
+UPF2AbilityTask_WaitForInitiativeTurn* UPF2AbilityTask_WaitForInitiativeTurn::CreateWaitInitiativeTurn(
+	UGameplayAbility*            OwningAbility,
+	const FName                  TaskInstanceName,
+	const FText                  ActionName,
+	const FSlateBrush            ActionIcon)
 {
 	UPF2AbilityTask_WaitForInitiativeTurn* Task =
 		NewAbilityTask<UPF2AbilityTask_WaitForInitiativeTurn>(OwningAbility, TaskInstanceName);
@@ -30,9 +30,33 @@ UPF2AbilityTask_WaitForInitiativeTurn* UPF2AbilityTask_WaitForInitiativeTurn::Cr
 	return Task;
 }
 
+UPF2AbilityTask_WaitForInitiativeTurn* UPF2AbilityTask_WaitForInitiativeTurn::CreateWaitInitiativeTurnWithTags(
+	UGameplayAbility*            OwningAbility,
+	const FName                  TaskInstanceName,
+	const FText                  ActionName,
+	const FSlateBrush            ActionIcon,
+	const FGameplayTagContainer& SourceTags,
+	const FGameplayTagContainer& TargetTags)
+{
+	UPF2AbilityTask_WaitForInitiativeTurn* Task =
+		NewAbilityTask<UPF2AbilityTask_WaitForInitiativeTurn>(OwningAbility, TaskInstanceName);
+
+	Task->ActionName = ActionName;
+	Task->ActionIcon = ActionIcon;
+	Task->SourceTags = &SourceTags;
+	Task->TargetTags = &TargetTags;
+
+	return Task;
+}
+
 void UPF2AbilityTask_WaitForInitiativeTurn::Activate()
 {
 	IPF2CharacterInterface* PF2Character = Cast<IPF2CharacterInterface>(this->GetOwnerActor());
+
+	checkf(
+		this->Ability->GetInstancingPolicy() == EGameplayAbilityInstancingPolicy::InstancedPerExecution,
+		TEXT("WaitForInitiativeTurn can only be used on abilites 'Instanced per Execution'.")
+	)
 
 	if ((PF2Character != nullptr) && this->HasAbility())
 	{
@@ -51,7 +75,7 @@ void UPF2AbilityTask_WaitForInitiativeTurn::Activate()
 
 void UPF2AbilityTask_WaitForInitiativeTurn::ExternalCancel()
 {
-	if (!this->WasActivated())
+	if (!this->WasActivatedOrCanceled())
 	{
 		if (this->ShouldBroadcastAbilityTaskDelegates())
 		{
@@ -64,7 +88,7 @@ void UPF2AbilityTask_WaitForInitiativeTurn::ExternalCancel()
 
 void UPF2AbilityTask_WaitForInitiativeTurn::OnDestroy(bool bAbilityEnded)
 {
-	if ((this->WaitingCharacter != nullptr) && (this->GameMode != nullptr) && !this->WasActivated())
+	if ((this->WaitingCharacter != nullptr) && (this->GameMode != nullptr) && !this->WasActivatedOrCanceled())
 	{
 		const TScriptInterface<IPF2CharacterInterface> CharacterScriptInterface =
 			this->WaitingCharacter.ToScriptInterface();
@@ -86,42 +110,77 @@ FSlateBrush UPF2AbilityTask_WaitForInitiativeTurn::GetActionIcon() const
 	return this->ActionIcon;
 }
 
+bool UPF2AbilityTask_WaitForInitiativeTurn::CanAbilityProceed() const
+{
+	const FGameplayAbilitySpecHandle AbilitySpecHandle = this->Ability->GetCurrentAbilitySpecHandle();
+	const FGameplayAbilityActorInfo  ActorInfo         = this->Ability->GetActorInfo();
+
+	// Prevent blocking ourselves.
+	this->Ability->SetShouldBlockOtherAbilities(false);
+
+	return this->Ability->CanActivateAbility(AbilitySpecHandle, &ActorInfo, this->SourceTags, this->TargetTags);
+}
+
 EPF2AbilityActivationResult UPF2AbilityTask_WaitForInitiativeTurn::PerformAction()
 {
-	if (this->HasAbility() && !this->IsPendingKill() && !this->WasActivated())
+	EPF2AbilityActivationResult Result = this->ActivationOutcome;
+
+	if (this->HasAbility() && !this->IsPendingKill() && !this->WasActivatedOrCanceled())
 	{
-		UE_LOG(
-			LogPf2CoreEncounters,
-			VeryVerbose,
-			TEXT("[%s] Performing action ('%s') for character ('%s')."),
-			*(PF2LogUtilities::GetHostNetId(this->GetWorld())),
-			*(this->GetActionName().ToString()),
-			*((this->WaitingCharacter != nullptr) ? this->WaitingCharacter->GetCharacterName().ToString() : TEXT("UNK"))
-		);
-
-		this->Ability->SetShouldBlockOtherAbilities(true);
-
-		if (this->ShouldBroadcastAbilityTaskDelegates())
+		if (this->CanAbilityProceed())
 		{
-			this->OnReadyToAct.Broadcast();
-		}
-
-		if (this->IsForRemoteClient() && this->HasAsc())
-		{
-			// Allow the client to proceed with its predicted copy of the ability.
-			this->AbilitySystemComponent->ClientSetReplicatedEvent(
-				EAbilityGenericReplicatedEvent::GenericSignalFromServer,
-				this->GetAbilitySpecHandle(),
-				this->GetActivationPredictionKey()
+			UE_LOG(
+				LogPf2CoreEncounters,
+				VeryVerbose,
+				TEXT("[%s] Performing action ('%s') for character ('%s')."),
+				*(PF2LogUtilities::GetHostNetId(this->GetWorld())),
+				*(this->GetActionName().ToString()),
+				*((this->WaitingCharacter != nullptr) ?
+					this->WaitingCharacter->GetCharacterName().ToString() :
+					TEXT("UNK"))
 			);
+
+			this->Ability->SetShouldBlockOtherAbilities(true);
+
+			if (this->ShouldBroadcastAbilityTaskDelegates())
+			{
+				this->OnReadyToAct.Broadcast();
+			}
+
+			if (this->IsForRemoteClient() && this->HasAsc())
+			{
+				// Allow the client to proceed with its predicted copy of the ability.
+				this->AbilitySystemComponent->ClientSetReplicatedEvent(
+					EAbilityGenericReplicatedEvent::GenericSignalFromServer,
+					this->GetAbilitySpecHandle(),
+					this->GetActivationPredictionKey()
+				);
+			}
+
+			Result = EPF2AbilityActivationResult::Activated;
+
+			this->EndTask();
+		}
+		else
+		{
+			UE_LOG(
+				LogPf2CoreEncounters,
+				VeryVerbose,
+				TEXT("[%s] Action ('%s') blocked for character ('%s')."),
+				*(PF2LogUtilities::GetHostNetId(this->GetWorld())),
+				*(this->GetActionName().ToString()),
+				*((this->WaitingCharacter != nullptr) ?
+					this->WaitingCharacter->GetCharacterName().ToString() :
+					TEXT("UNK"))
+			);
+
+			Result = EPF2AbilityActivationResult::Blocked;
 		}
 
-		this->ActivationOutcome = EPF2AbilityActivationResult::Activated;
-
-		this->EndTask();
+		this->ActivationOutcome = Result;
 	}
 
-	return this->ActivationOutcome;
+	return Result;
 }
 
 void UPF2AbilityTask_WaitForInitiativeTurn::CancelAction()
@@ -172,17 +231,21 @@ void UPF2AbilityTask_WaitForInitiativeTurn::Activate_Server(IPF2CharacterInterfa
 			switch (QueueResult)
 			{
 			case EPF2ActionQueueResult::Refused:
+				// The MoPRS refused to allow this action at all (rare).
 				this->CancelAction();
 				break;
 
 			case EPF2ActionQueueResult::ExecutedImmediately:
-				// Nothing to do.
+				// The MoPRS already executed the action without any queueing, so we have nothing left to do.
 				break;
 
 			default:
 			case EPF2ActionQueueResult::Queued:
+				// The MoPRS queued the action for later execution. Notify the ability and temporarily unblock this
+				// ability from blocking other abilities that would otherwise be incompatible.
 				this->OnQueued.Broadcast();
 				this->Ability->SetShouldBlockOtherAbilities(false);
+
 				this->SetWaitingOnRemotePlayerData();
 				break;
 			}
--- Source/OpenPF2Core/Public/Abilities/Tasks/PF2AbilityTask_WaitForInitiativeTurn.h
+++ Source/OpenPF2Core/Public/Abilities/Tasks/PF2AbilityTask_WaitForInitiativeTurn.h
@@ -21,7 +21,10 @@
  * A task to delay execution of a Gameplay Ability until it is the character's turn according to initiative.
  *
  * Only GAs activated in encounter mode are subject to initiative. GAs activated while the game is in exploration or
- * downtime mode are executed immediately, without delay.
+ * downtime mode are typically executed immediately, without delay.
+ *
+ * This ability task can only be used with GAs that are instanced per execution. This is necessary so that a GA can be
+ * queued multiple times and each queued activation does not affect the other activations.
  */
 UCLASS()
 // ReSharper disable once CppClassCanBeFinal
@@ -40,6 +43,9 @@ public:
 	 *
 	 * This only affects the GA if it is activated in encounter mode. If this GA is activated while the game is in
 	 * exploration or downtime mode, it is executed immediately, without delay.
+	 *
+	 * The GA is only "resumed" after being queued if the ability is not otherwise blocked (e.g., but other abilities
+	 * that are active on the character at the time that it is being resumed).
 	 *
 	 * Execution pins work as follows:
 	 *	- OnQueued is called if the ability is queued, in case it needs to suspend itself while it is queued.
@@ -68,10 +74,63 @@ public:
 			DisplayName="Wait for Initiative Turn"
 		)
 	)
-	static UPF2AbilityTask_WaitForInitiativeTurn* CreateWaitMovementModeChange(UGameplayAbility* OwningAbility,
-	                                                                           const FName       TaskInstanceName,
-	                                                                           const FText       ActionName,
-	                                                                           const FSlateBrush ActionIcon);
+	static UPF2AbilityTask_WaitForInitiativeTurn* CreateWaitInitiativeTurn(
+		UGameplayAbility*           OwningAbility,
+		const FName                 TaskInstanceName,
+		const FText                 ActionName,
+		const FSlateBrush           ActionIcon);
+
+	/**
+	 * Delay execution of this Gameplay Ability until the character's initiative turn comes up and tags match.
+	 *
+	 * This only affects the GA if it is activated in encounter mode. If this GA is activated while the game is in
+	 * exploration or downtime mode, it is executed immediately, without delay.
+	 *
+	 * The GA is only "resumed" after being queued if the ability is not otherwise blocked (e.g., but other abilities
+	 * that are active on the character at the time that it is being resumed). In addition, this variation of the
+	 * "Wait for Initiative Turn" task also factors in the tags of both the source/instigator and the target, and is
+	 * intended to be used by abilities activated from events. The tags are evaluated at the time that the GA is being
+	 * dequeued, so they should be provided from "live" objects rather than snapshotted objects to ensure that the tags
+	 * are an accurate reflection of the world.
+	 *
+	 * Execution pins work as follows:
+	 *	- OnQueued is called if the ability is queued, in case it needs to suspend itself while it is queued.
+	 *	- OnReadyToAct is called when the character's "turn" in initiative order comes up. If the current Mode of Play
+	 *	  Rule Set does not queue actions, this will fire immediately.
+	 *	- OnCancelled is called if the ability or task is cancelled, by either a player or the rule set (e.g., due to a
+	 *	  change in mode of play).
+	 *
+	 * @param OwningAbility
+	 *	A reference to the ability instance that is triggering this task. (This is automatically populated by Blueprint
+	 *	from the ability Blueprint.)
+	 * @param TaskInstanceName
+	 *	A custom name for this task, in case it needs to be queried later while it is running.
+	 * @param ActionName
+	 *	The name to display for this action when queued, if the queue of actions are displayed to the player/user.
+	 * @param ActionIcon
+	 *	The icon to display for this action when queued, if the queue of actions are displayed to the player/user.
+	 * @param SourceTags
+	 *	The tags from the source/instigator actor/character. Can be left empty if the ability does not use source tags.
+	 * @param TargetTags
+	 *	The tags from the target actor/character. Can be left empty if this ability does not use target tags.
+	 */
+	UFUNCTION(
+		BlueprintCallable,
+		Category="Ability|Tasks",
+		meta=(
+			HidePin="OwningAbility",
+			DefaultToSelf="OwningAbility",
+			BlueprintInternalUseOnly="TRUE",
+			DisplayName="Wait for Initiative Turn with Tags"
+		)
+	)
+	static UPF2AbilityTask_WaitForInitiativeTurn* CreateWaitInitiativeTurnWithTags(
+		UGameplayAbility*           OwningAbility,
+		const FName                 TaskInstanceName,
+		const FText                 ActionName,
+		const FSlateBrush           ActionIcon,
+		const FGameplayTagContainer& SourceTags,
+		const FGameplayTagContainer& TargetTags);
 
 protected:
 	// =================================================================================================================
@@ -98,6 +157,20 @@ protected:
 	// =================================================================================================================
 	// Protected Fields
 	// =================================================================================================================
+	/**
+	 * For abilities that use source tags: The tags from the source/instigator actor/character.
+	 *
+	 * Can be a nullptr or empty container if the ability does not use source tags.
+	 */
+	const FGameplayTagContainer* SourceTags;
+
+	/**
+	 * For abilities that use target tags: The tags from the target actor/character.
+	 *
+	 * Can be a nullptr or empty container if the ability does not use target tags.
+	 */
+	const FGameplayTagContainer* TargetTags;
+
 	/**
 	 * The character who is waiting on a turn to continue the GA that invoked this task.
 	 */
@@ -171,15 +244,20 @@ protected:
 	// Protected Methods
 	// =================================================================================================================
 	/**
-	 * Gets whether this ability task was executed after being queued.
+	 * Gets whether this ability task was executed or cancelled after being queued.
 	 *
 	 * @return
-	 *	- true if this ability task was executed after being queued.
-	 *	- false if this ability task has not yet been queued and/or executed.
+	 *	- true if this ability task was executed or cancelled.
+	 *	- false if this ability task has not yet been executed or cancelled.
 	 */
-	FORCEINLINE bool WasActivated() const
+	FORCEINLINE bool WasActivatedOrCanceled() const
 	{
-		return (this->ActivationOutcome != EPF2AbilityActivationResult::None);
+		const TArray<EPF2AbilityActivationResult> Statuses = {
+			EPF2AbilityActivationResult::Activated,
+			EPF2AbilityActivationResult::Cancelled,
+		};
+
+		return (Statuses.Contains(this->ActivationOutcome));
 	}
 
 	/**
@@ -208,4 +286,17 @@ protected:
 	 * This method does not get called on the server side; it only runs on the client side.
 	 */
 	void OnPerformAction_Client();
+
+	/**
+	 * Determine if the owning ability is blocked from activation or can be resumed where it left off.
+	 *
+	 * This takes the latest source and target tags into account because they may have changed since the ability was
+	 * queued. For example, if this was an area of affect ability for which the player chose a target area, we need to
+	 * recheck that the tags of the targets have not changed since the ability was queued.
+	 *
+	 * @return
+	 *	- true if the ability is unblocked and can be resumed.
+	 *	- false if the ability is blocked and cannot be resumed at this time (may be resumed in the future).
+	 */
+	bool CanAbilityProceed() const;
 };
