e8e9833d546e5fe9f4fbe5b9aa0792c88fae5ffc
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2022-02-26 19:30:15 -0500

    [#21] Filter Out Null Controlled Characters
    Although unlikely, `GetControlledCharacter()` can, in fact, return
    `nullptr`.

--- Source/OpenPF2Core/Private/GameModes/PF2ModeOfPlayRuleSetBase.cpp
+++ Source/OpenPF2Core/Private/GameModes/PF2ModeOfPlayRuleSetBase.cpp
@@ -64,10 +64,13 @@ TArray<TScriptInterface<IPF2PlayerControllerInterface>> UPF2ModeOfPlayRuleSetBas
 
 TArray<TScriptInterface<IPF2CharacterInterface>> UPF2ModeOfPlayRuleSetBase::GetPlayerControlledCharacters() const
 {
-	return PF2ArrayUtilities::Map<TScriptInterface<IPF2CharacterInterface>>(
-		this->GetPlayerControllers(),
-		[](const TScriptInterface<IPF2PlayerControllerInterface> PlayerController)
-		{
-			return PlayerController->GetControlledCharacter();
-		});
+	return PF2ArrayUtilities::Filter<TScriptInterface<IPF2CharacterInterface>>(
+		PF2ArrayUtilities::Map<TScriptInterface<IPF2CharacterInterface>>(
+			this->GetPlayerControllers(),
+			[](const TScriptInterface<IPF2PlayerControllerInterface> PlayerController)
+			{
+				return PlayerController->GetControlledCharacter();
+			}
+		)
+	);
 }
