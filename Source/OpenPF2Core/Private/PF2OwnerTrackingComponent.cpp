56d15c9e62eec5f59e693bff518cfd6b23e90a1d
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2022-08-24 23:20:04 -0400

    [#23] Soften Licensing Language Regarding UE RTS
    This code is likely to evolve far beyond what we borrowed from the RTS
    project, so let's start our licensing verbiage off on the right foot.

--- Source/OpenPF2Core/Private/PF2OwnerTrackingComponent.cpp
+++ Source/OpenPF2Core/Private/PF2OwnerTrackingComponent.cpp
@@ -3,8 +3,8 @@
 // This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 // distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 //
-// This code is adapted from the "Real-Time Strategy Plugin for Unreal Engine 4" by Nick Pruehs, provided under the MIT
-// License. Copyright (c) 2017 Nick Pruehs.
+// Portions of this code were adapted from or inspired by the "Real-Time Strategy Plugin for Unreal Engine 4" by Nick
+// Pruehs, provided under the MIT License. Copyright (c) 2017 Nick Pruehs.
 //
 
 #include "PF2OwnerTrackingComponent.h"
