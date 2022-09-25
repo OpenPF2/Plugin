4398b4925d5131f35d4f1e24d29f456a4d7dd8a5
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2022-09-25 17:59:32 -0400

    [#23] Address PR Feedback -- Update Copyrights

--- Source/OpenPF2Core/Private/Tests/PF2SpecBase.h
+++ Source/OpenPF2Core/Private/Tests/PF2SpecBase.h
@@ -1,4 +1,4 @@
-﻿// Copyright 2021 Guy Elsmore-Paddock. All Rights Reserved.
+﻿// Copyright 2021-2022 Guy Elsmore-Paddock. All Rights Reserved.
 // Adapted from content that is Copyright Epic Games, Inc. (Action RPG Sample).
 // Licensed only for use with Unreal Engine.
 
