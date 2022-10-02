96ec639aa49a8e63f596fdedfee2d4a168150af2
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2022-10-02 02:15:52 -0400

    [#22] Implement `UPF2ActorLibrary::GetComponentByInterface()`
    Surprisingly, there is no built-in already defined for getting only a
    single component by its interface. This addresses that shortcoming to
    make it possible for game designers to get components by interface in
    much fewer steps. Without this, they have to use
    `GetComponentsByInterface`, do a length check, grab the first element,
    and then cast it.

---
+++ Source/OpenPF2Core/Private/Libraries/PF2ActorLibrary.cpp
@@ -0,0 +1,33 @@
+﻿// OpenPF2 for UE Game Logic, Copyright 2022, Guy Elsmore-Paddock. All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+// distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "Libraries/PF2ActorLibrary.h"
+
+#include <Components/ActorComponent.h>
+
+#include <GameFramework/Actor.h>
+
+#include <Templates/SubclassOf.h>
+
+TScriptInterface<IInterface> UPF2ActorLibrary::GetComponentByInterface(const AActor*                 Actor,
+                                                                       const TSubclassOf<UInterface> Interface)
+{
+	TScriptInterface<IInterface>   Result;
+	const TArray<UActorComponent*> Components = Actor->GetComponentsByInterface(Interface);
+
+	checkf(
+		Components.Num() < 2,
+		TEXT("More than one component implements the same interface: %s"),
+		*(Interface->GetName())
+	);
+
+	for (UActorComponent* Component : Components)
+	{
+		Result = Component;
+		break;
+	}
+
+	return Result;
+}
