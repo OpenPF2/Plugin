9c990788fb0858863b53dde118a5db4eea2055f4
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2022-10-30 22:43:54 -0400

    [#22] Fix File License
    Nothing in this is OGL-sourced.

--- Source/OpenPF2Core/Public/PF2CharacterQueueComponent.h
+++ Source/OpenPF2Core/Public/PF2CharacterQueueComponent.h
@@ -1,14 +1,7 @@
 ﻿// OpenPF2 for UE Game Logic, Copyright 2022, Guy Elsmore-Paddock. All Rights Reserved.
 //
-// Content from Pathfinder 2nd Edition is licensed under the Open Game License (OGL) v1.0a, subject to the following:
-//   - Open Game License v 1.0a, Copyright 2000, Wizards of the Coast, Inc.
-//   - System Reference Document, Copyright 2000, Wizards of the Coast, Inc.
-//   - Pathfinder Core Rulebook (Second Edition), Copyright 2019, Paizo Inc.
-//
-// Except for material designated as Product Identity, the game mechanics and logic in this file are Open Game Content,
-// as defined in the Open Game License version 1.0a, Section 1(d) (see accompanying LICENSE.TXT). No portion of this
-// file other than the material designated as Open Game Content may be reproduced in any form without written
-// permission.
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+// distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 #pragma once
 
