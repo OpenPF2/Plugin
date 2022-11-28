786b41ea1e1664ec4dd06bcf452193b647e978de
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2022-11-27 23:42:32 -0500

    [#21] Expand OpenPF2-compatible Ability Task Base Class
    This makes it easier for sub-classes to get OpenPF2-compatible versions
    of the ability, ASC, and character ASC without as much boilerplate code.

---
+++ Source/OpenPF2Core/Private/Abilities/Tasks/PF2AbilityTaskBase.cpp
@@ -0,0 +1,54 @@
+﻿// OpenPF2 for UE Game Logic, Copyright 2022, Guy Elsmore-Paddock. All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+// distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#include "Abilities/Tasks/PF2AbilityTaskBase.h"
+
+#include "Abilities/PF2AbilitySystemInterface.h"
+#include "Abilities/PF2CharacterAbilitySystemInterface.h"
+#include "Abilities/PF2GameplayAbilityInterface.h"
+
+#include "Utilities/PF2InterfaceUtilities.h"
+
+TScriptInterface<IPF2GameplayAbilityInterface> UPF2AbilityTaskBase::GetAbility() const
+{
+	IPF2GameplayAbilityInterface* AbilityIntf = Cast<IPF2GameplayAbilityInterface>(this->GetNativeAbility());
+
+	if (AbilityIntf == nullptr)
+	{
+		return TScriptInterface<IPF2GameplayAbilityInterface>(nullptr);
+	}
+	else
+	{
+		return PF2InterfaceUtilities::ToScriptInterface(AbilityIntf);
+	}
+}
+
+TScriptInterface<IPF2AbilitySystemInterface> UPF2AbilityTaskBase::GetAbilitySystemComponent() const
+{
+	IPF2AbilitySystemInterface* AscIntf = Cast<IPF2AbilitySystemInterface>(this->GetNativeAbilitySystemComponent());
+
+	if (AscIntf == nullptr)
+	{
+		return TScriptInterface<IPF2AbilitySystemInterface>(nullptr);
+	}
+	else
+	{
+		return PF2InterfaceUtilities::ToScriptInterface(AscIntf);
+	}
+}
+
+TScriptInterface<IPF2CharacterAbilitySystemInterface> UPF2AbilityTaskBase::GetCharacterAbilitySystemComponent() const
+{
+	IPF2CharacterAbilitySystemInterface* AscIntf = Cast<IPF2CharacterAbilitySystemInterface>(this->GetNativeAbilitySystemComponent());
+
+	if (AscIntf == nullptr)
+	{
+		return TScriptInterface<IPF2CharacterAbilitySystemInterface>(nullptr);
+	}
+	else
+	{
+		return PF2InterfaceUtilities::ToScriptInterface<IPF2CharacterAbilitySystemInterface>(AscIntf);
+	}
+}
--- Source/OpenPF2Core/Public/Abilities/Tasks/PF2AbilityTaskBase.h
+++ Source/OpenPF2Core/Public/Abilities/Tasks/PF2AbilityTaskBase.h
@@ -9,6 +9,17 @@
 
 #include "PF2AbilityTaskBase.generated.h"
 
+// =====================================================================================================================
+// Forward Declarations (to minimize header dependencies)
+// =====================================================================================================================
+class IPF2AbilitySystemInterface;
+class IPF2CharacterAbilitySystemInterface;
+class IPF2GameplayAbilityInterface;
+class UAbilitySystemComponent;
+
+// =====================================================================================================================
+// Normal Declarations
+// =====================================================================================================================
 /**
  * Common base class for OpenPF2 Ability Tasks.
  */
@@ -31,13 +42,70 @@ protected:
 	}
 
 	/**
+	 * Gets the ability that is executing this ability task.
+	 *
+	 * @return
+	 *	Either the owning ability; or null if the ability of this ability task
+	 *	has not yet been initialized.
+	 */
+	FORCEINLINE UGameplayAbility* GetNativeAbility() const
+	{
+		check(this->HasAbility());
+
+		return this->Ability;
+	}
+
+	/**
+	 * Gets the OpenPF2-compatible interface for the ability that is invoking this ability task.
+	 *
+	 * @return
+	 *	The ability as a PF2 ability script interface instance. If the ability is unset or not OpenPF2-compatible, the
+	 *	script interface wraps nullptr.
+	 */
+	UFUNCTION(BlueprintCallable)
+	TScriptInterface<IPF2GameplayAbilityInterface> GetAbility() const;
+
+	/**
 	 * Gets whether this ability task has a valid Ability System Component.
 	 *
 	 * @return
 	 *	true if the ASC in this task is properly set; or false if it is null.
 	 */
-	FORCEINLINE bool HasAsc() const
+	FORCEINLINE bool HasAbilitySystemComponent() const
 	{
 		return this->AbilitySystemComponent != nullptr;
 	}
+
+	/**
+	 * Gets the Ability System Component of this ability task.
+	 *
+	 * @return
+	 *	The ASC in this task.
+	 */
+	FORCEINLINE UAbilitySystemComponent* GetNativeAbilitySystemComponent() const
+	{
+		check(this->HasAbilitySystemComponent());
+
+		return this->AbilitySystemComponent;
+	}
+
+	/**
+	 * Gets the OpenPF2-compatible interface for the ability system component invoking this ability task.
+	 *
+	 * @return
+	 *	The ASC as a PF2 ASC script interface instance. If the ASC is unset or not OpenPF2-compatible, the script
+	 *	interface wraps nullptr.
+	 */
+	UFUNCTION(BlueprintCallable)
+	TScriptInterface<IPF2AbilitySystemInterface> GetAbilitySystemComponent() const;
+
+	/**
+	 * Gets the OpenPF2-compatible interface for the character ability system component invoking this ability task.
+	 *
+	 * @return
+	 *	The ASC as a PF2 character ASC script interface instance. If the ASC is unset or not an OpenPF2 character ASC,
+	 *	the script interface wraps nullptr.
+	 */
+	UFUNCTION(BlueprintCallable)
+	TScriptInterface<IPF2CharacterAbilitySystemInterface> GetCharacterAbilitySystemComponent() const;
 };
