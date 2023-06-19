3c7f35c1d90d53fde65f72a5c26f303dddd94942
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2023-06-19 01:08:06 -0400

    [#8] Make it Possible to Locate an Ability Spec by Ability Instance
    This makes it possible to convert from a Gameplay Ability (GA) instance
    into the spec for the GA from the character's Ability System Component.
    This makes it much easier to invoke an ability on a character by its
    type rather than having the spec on hand.

--- Source/OpenPF2Core/Private/Abilities/PF2GameplayAbilityBase.cpp
+++ Source/OpenPF2Core/Private/Abilities/PF2GameplayAbilityBase.cpp
@@ -34,6 +34,64 @@ FName UPF2GameplayAbilityBase::GetDefaultInputActionMapping() const
 	return this->DefaultInputActionMapping;
 }
 
+FGameplayAbilitySpec UPF2GameplayAbilityBase::ToGameplayAbilitySpecForCharacter(
+	const TScriptInterface<IPF2CharacterInterface> Character,
+	bool& bOutSpecFound
+) const
+{
+	FGameplayAbilitySpec AbilitySpec;
+
+	if (Character == nullptr)
+	{
+		UE_LOG(
+			LogPf2CoreAbilities,
+			Error,
+			TEXT("Given null for character when attempting to find ability spec for ability ('%s')."),
+			*(this->GetIdForLogs())
+		);
+	}
+	else
+	{
+		UAbilitySystemComponent* AbilitySystemComponent = Character->GetAbilitySystemComponent();
+		bool                     bFoundAbilitySpec      = false;
+
+		check(AbilitySystemComponent != nullptr);
+
+		for (const FGameplayAbilitySpec& CurrentAbilitySpec : AbilitySystemComponent->GetActivatableAbilities())
+		{
+			if (CurrentAbilitySpec.Ability.Get() == this)
+			{
+				AbilitySpec       = CurrentAbilitySpec;
+				bFoundAbilitySpec = true;
+				break;
+			}
+		}
+
+		if (!bFoundAbilitySpec)
+		{
+			UE_LOG(
+				LogPf2CoreAbilities,
+				Error,
+				TEXT("Failed to find ability spec in ASC for ability ('%s') granted to character ('%s')."),
+				*(this->GetIdForLogs()),
+				*(Character->GetIdForLogs())
+			);
+		}
+
+		bOutSpecFound = bFoundAbilitySpec;
+	}
+
+	return AbilitySpec;
+}
+
+FGameplayAbilitySpecHandle UPF2GameplayAbilityBase::ToGameplayAbilitySpecHandleForCharacter(
+	const TScriptInterface<IPF2CharacterInterface> Character,
+	bool&                                          bOutSpecHandleFound
+) const
+{
+	return this->ToGameplayAbilitySpecForCharacter(Character, bOutSpecHandleFound).Handle;
+}
+
 TScriptInterface<IPF2CharacterInterface> UPF2GameplayAbilityBase::GetOwningCharacterFromActorInfo() const
 {
 	TScriptInterface<IPF2CharacterInterface> Result;
