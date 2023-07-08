e77ffc505b4a5980e1d7cab9a60b5c685e924912
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2023-07-08 00:55:44 -0400

    [#8] WIP Editor Support Module for Enhancing DX for Intf. Events Objects
    The new pattern of having UInterfaces expose an "events" object for
    binding delegates through an interface is great for avoiding coupling to
    a concrete implementation of an interface, but it adds friction when
    trying to wire-up delegates while in a Blueprint that instantiates an
    interface implementation (i.e., if you are in a `PF2PlayerController`
    blueprint and the controller has a `PF2CharacterQueueComponent`, it
    already knows the concrete type of the component and should be able to
    place the event bindings directly in the Blueprint without having to
    bind delegates through the `GetEvents()` method).
    
    To fix this, a new editor support module has been added that will use
    the `PF2EventsInterface` to reflect on what events an interface object
    is exposing so that bindings can be added to those delegates as if they
    were bound on the interface object itself rather than its events object.
    
    Right now, events appear correctly in the "Details" panel of the editor,
    but you cannot yet bind them in the Blueprint (the + button does nothing
    yet).

---
+++ Source/OpenPF2EditorSupport/Public/InterfaceEventsDetailCustomization.h
@@ -0,0 +1,76 @@
+﻿// OpenPF2 for UE Game Logic, Copyright 2023, Guy Elsmore-Paddock. All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+// distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#pragma once
+
+#include <IDetailCustomization.h>
+#include <SMyBlueprint.h>
+#include <SSubobjectEditor.h>
+
+#include <Input/Reply.h>
+
+class IPF2EventsInterface;
+
+// ReSharper disable once CppClassCanBeFinal
+class FInterfaceEventsDetailCustomization : public IDetailCustomization
+{
+public:
+	// =================================================================================================================
+	// Public Static Methods
+	// =================================================================================================================
+	/**
+	 * Makes a new instance of this detail layout class for a specific detail view requesting it.
+	 */
+	static TSharedRef<IDetailCustomization> MakeInstance()
+	{
+		return MakeShareable(new FInterfaceEventsDetailCustomization());
+	}
+
+protected:
+	// =================================================================================================================
+	// Protected Fields
+	// =================================================================================================================
+	TWeakObjectPtr<UBlueprint> Blueprint;
+
+public:
+	explicit FInterfaceEventsDetailCustomization() : Blueprint(nullptr)
+	{
+	}
+
+	// =================================================================================================================
+	// Public Methods - IDetailCustomization Implementation
+	// =================================================================================================================
+	virtual void CustomizeDetails(IDetailLayoutBuilder& DetailBuilder) override;
+
+	// =================================================================================================================
+	// Protected Instance Methods
+	// =================================================================================================================
+	FORCEINLINE UBlueprint* GetBlueprint() const
+	{
+		return Blueprint.Get();
+	}
+
+protected:
+	void AddEventsCategory(IDetailLayoutBuilder& DetailBuilder,
+	                       const UObject*        SelectedObject,
+	                       const FName           SelectedVariableName) const;
+
+	FReply HandleAddOrViewEventForVariable(const FName                  EventName,
+	                                       const FName                  PropertyName,
+	                                       const TWeakObjectPtr<UClass> PropertyClass) const;
+
+	int32 GetAddOrViewIndexForButton(const FName EventName,
+	                                 const FName PropertyName) const;
+
+	static UBlueprint* GetBlueprintContainingObject(const UObject* SelectedObject);
+
+	static FSubobjectEditorTreeNodePtrType GetSelectedTreeNodeInBlueprint(const UBlueprint* Blueprint);
+
+	static TSharedPtr<SSubobjectEditor> GetSubobjectEditorForBlueprint(const UBlueprint* Blueprint);
+
+	static FBlueprintEditor* GetBlueprintEditorForBlueprint(const UBlueprint* Blueprint);
+
+	static FBlueprintEditor* GetBlueprintEditorForGraph(const UEdGraph* Graph);
+};
