77058fa8bc5f900ced3dd3744a943ef0b45d292d
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2023-07-25 22:41:53 -0400

    [#8] Update Copyrights

--- Source/OpenPF2Core/Private/PF2EventEmitterDelegateBinding.cpp
+++ Source/OpenPF2Core/Private/PF2EventEmitterDelegateBinding.cpp
@@ -1,7 +1,7 @@
 // OpenPF2 for UE Game Logic, Copyright 2023, Guy Elsmore-Paddock. All Rights Reserved.
 //
-// Adapted from "Runtime/Engine/Private/ComponentDelegateBinding.cpp" (Unreal Engine 5.1), which is Copyright Epic
-// Games, Inc. Licensed only for use with Unreal Engine.
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+// distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 #include "PF2EventEmitterDelegateBinding.h"
 
