233dd5d2c20e3e891541102cf4bfee607392d5d6
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2023-11-14 23:53:33 -0500

    [#53] Add Specs for New Attack Attribute Set

--- Source/OpenPF2Core/Private/Tests/PF2TestPawn.cpp
+++ Source/OpenPF2Core/Private/Tests/PF2TestPawn.cpp
@@ -1,18 +1,19 @@
-﻿// OpenPF2 for UE Game Logic, Copyright 2021, Guy Elsmore-Paddock. All Rights Reserved.
+﻿// OpenPF2 for UE Game Logic, Copyright 2021-2023, Guy Elsmore-Paddock. All Rights Reserved.
 //
 // This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 // distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 #include "Tests/PF2TestPawn.h"
 #include <AbilitySystemComponent.h>
+
+#include "Abilities/PF2AttackAttributeSet.h"
 #include "Abilities/PF2CharacterAttributeSet.h"
 
 FName APF2TestPawn::AbilitySystemComponentName(TEXT("AbilitySystemComponent0"));
 
 APF2TestPawn::APF2TestPawn(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
-	this->AbilitySystemComponent =
-		this->CreateDefaultSubobject<UAbilitySystemComponent>(APF2TestPawn::AbilitySystemComponentName);
+	this->AbilitySystemComponent = this->CreateDefaultSubobject<UAbilitySystemComponent>(AbilitySystemComponentName);
 
 	this->AbilitySystemComponent->SetIsReplicated(true);
 }
@@ -22,6 +23,7 @@ void APF2TestPawn::PostInitializeComponents()
 	Super::PostInitializeComponents();
 
 	this->AbilitySystemComponent->InitStats(UPF2CharacterAttributeSet::StaticClass(), nullptr);
+	this->AbilitySystemComponent->InitStats(UPF2AttackAttributeSet::StaticClass(), nullptr);
 }
 
 UAbilitySystemComponent* APF2TestPawn::GetAbilitySystemComponent() const
