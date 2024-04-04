901b3645b26ebfdbe9c3fa99ca660021409e1a37
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2024-04-04 18:38:11 -0400

    [#53] [PRF] Remove Unnecessary Note about UE RTS from Actor Comp. Base
    None of *this* code came from UE RTS.

--- Source/OpenPF2Core/Private/Actors/Components/PF2ActorComponentBase.cpp
+++ Source/OpenPF2Core/Private/Actors/Components/PF2ActorComponentBase.cpp
@@ -3,9 +3,6 @@
 // This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 // distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
 //
-// Portions of this code were adapted from or inspired by the "Real-Time Strategy Plugin for Unreal Engine 4" by Nick
-// Pruehs, provided under the MIT License. Copyright (c) 2017 Nick Pruehs.
-//
 
 #include "Actors/Components/PF2ActorComponentBase.h"
 
