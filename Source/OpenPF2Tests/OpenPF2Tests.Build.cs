03e9748254f68c3a96182dab18b64833d0af77d9
Guy Elsmore-Paddock <guy.paddock@gmail.com>
2024-06-16 22:55:19 -0400

    [#62] Switch to Enh. Specs. Plug in + Move Tests to `OpenPF2Tests` Mod.
    Now, tests for the project live in a separate module that is only built
    for uncooked builds. This is necessary because cooked builds appear to
    not have automation test support (which makes some intuitive sense,
    since automation tests would bloat the final product for no benefit
    other than smoke testing).

---
+++ Source/OpenPF2Tests/OpenPF2Tests.Build.cs
@@ -0,0 +1,28 @@
+// OpenPF2 for UE Game Logic, Copyright 2024, Guy Elsmore-Paddock. All Rights Reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
+// distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+using UnrealBuildTool;
+
+// ReSharper disable once InconsistentNaming
+public class OpenPF2Tests : ModuleRules
+{
+	public OpenPF2Tests(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+
+		PublicDependencyModuleNames.AddRange(
+			new[]
+			{
+				"Core",
+				"CoreUObject",
+				"Engine",
+				"GameplayAbilities",
+				"GameplayTags",
+				"EnhancedAutomationSpecFramework",
+				"OpenPF2Core",
+			}
+		);
+	}
+}
--- Source/OpenPF2Tests/Public/Tests/PF2TestAbility.h
+++ Source/OpenPF2Tests/Public/Tests/PF2TestAbility.h
@@ -1,4 +1,4 @@
-// OpenPF2 for UE Game Logic, Copyright 2023, Guy Elsmore-Paddock. All Rights Reserved.
+// OpenPF2 for UE Game Logic, Copyright 2023-2024, Guy Elsmore-Paddock. All Rights Reserved.
 //
 // This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not
 // distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
@@ -13,7 +13,7 @@
  * A fake Gameplay Ability for use in testing logic that requires abilities granted to a character.
  */
 UCLASS(Blueprintable, BlueprintType, notplaceable)
-class OPENPF2CORE_API UPF2TestAbility : public UGameplayAbility
+class OPENPF2TESTS_API UPF2TestAbility : public UGameplayAbility
 {
 	GENERATED_BODY()
 };
